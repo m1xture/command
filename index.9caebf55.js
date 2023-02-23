e6c8be04bef027f3071954cff8088ff3a4aa5477
m1xture <109725502+m1xture@users.noreply.github.com>
2023-02-23 18:47:51 +0000

    Deploying to gh-pages from @ m1xture/command@e4ef659f2fdbcc8cf709b81ac87a6961dcd7d91e 🚀

---
+++ index.9caebf55.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-mobile-open]"),closeModalBtn:document.querySelector("[data-mobile-close]"),modal:document.querySelector("[data-mobile]")};function o(){e.modal.classList.toggle("is-close")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.9caebf55.js.map
